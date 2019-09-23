c9e8ccd410f7a74fa4fa41499657307d28a205ec
3BK <40867233+3BK@users.noreply.github.com>
2019-09-23 16:48:02 -0400

    Update getHyperStats.sh

--- getHyperStats.sh
+++ getHyperStats.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+echo "datetime,fqdn,tag,measure"
 vmware-toolbox-cmd stat raw | xargs -n 1 -I '{}' sh -c 'prefix=$(echo "{}"| sed "s/^[ \t]*//" | sed "s/[ \t]*$//" | sed "s/ /./"); vmware-toolbox-cmd stat raw text {} | awk -v dt=$(date +\%d/\%m/\%YT\%H:\%M:\%S\%Z) -v hst=$(hostname -A)  -v pfx=$prefix "function ltrim(s) { sub(/^[ \t\r\n]+/, \"\", s); return s } function rtrim(s) { sub(/[ \t\r\n]+$/, \"\", s); return s } function trim(s)  { return rtrim(ltrim(s)); } BEGIN{FS=\"=\"} {print dt\",\"hst\",\"pfx\".\"trim(\$1)\",\"trim(\$2)}" '
