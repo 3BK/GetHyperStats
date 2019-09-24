4b7d1d79462712d01493230acd172e24d3bd5645
3BK <40867233+3BK@users.noreply.github.com>
2019-09-24 15:58:04 -0400

    Update getHyperStats.sh

--- getHyperStats.sh
+++ getHyperStats.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 HOSTNAME=$(hostname -A)
 
-#TODO split out units
+#TODO: split out units
+#TODO: fix datetime layout
+
 echo "datetime,fqdn,tag,measure"
 
 printf "%s\n" hosttime speed sessionid balloon swap memlimit memres cpures cpulimit | xargs -n 1 -I '{}' sh -c 'prefix=$(echo "{}"| sed "s/^[ \t]*//" | sed "s/[ \t]*$//" | sed "s/ /./"); vmware-toolbox-cmd stat {} | awk -v dt=$(date +\%d/\%m/\%YT\%H:\%M:\%S\%Z) -v hst=$HOSTNAME  -v pfx=$prefix "function ltrim(s) { sub(/^[ \t\r\n]+/, \"\", s); return s } function rtrim(s) { sub(/[ \t\r\n]+$/, \"\", s); return s } function trim(s)  { return rtrim(ltrim(s)); } BEGIN{FS=\"=\"} { if(NF==2) {print dt\",\"hst\",\"pfx\".\"trim(\$1)\",\"trim(\$2)} if(NF==1)  {print dt\",\"hst\",\"pfx\",\"trim(\$1)} } " '
