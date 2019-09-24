f27d3668a93c967f72c835ecc70b4c9fc3906a91
3BK <40867233+3BK@users.noreply.github.com>
2019-09-24 15:57:02 -0400

    Update getHyperStats.sh

--- getHyperStats.sh
+++ getHyperStats.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 HOSTNAME=$(hostname -A)
 
-#TODO: track units.
-
+#TODO split out units
 echo "datetime,fqdn,tag,measure"
 
-echo "hosttime speed sessionid balloon swap memlimit memres cpures cpulimit" | xargs -n 1 -I '{}' sh -c 'prefix=$(echo "{}"| sed "s/^[ \t]*//" | sed "s/[ \t]*$//" | sed "s/ /./"); vmware-toolbox-cmd stat {} | awk -v dt=$(date +\%d/\%m/\%YT\%H:\%M:\%S\%Z) -v hst=$HOSTNAME  -v pfx=$prefix "function ltrim(s) { sub(/^[ \t\r\n]+/, \"\", s); return s } function rtrim(s) { sub(/[ \t\r\n]+$/, \"\", s); return s } function trim(s)  { return rtrim(ltrim(s)); } BEGIN{FS=\"=\"} {print dt\",\"hst\",\"pfx\".\"trim(\$1)\",\"trim(\$2)}" '
+printf "%s\n" hosttime speed sessionid balloon swap memlimit memres cpures cpulimit | xargs -n 1 -I '{}' sh -c 'prefix=$(echo "{}"| sed "s/^[ \t]*//" | sed "s/[ \t]*$//" | sed "s/ /./"); vmware-toolbox-cmd stat {} | awk -v dt=$(date +\%d/\%m/\%YT\%H:\%M:\%S\%Z) -v hst=$HOSTNAME  -v pfx=$prefix "function ltrim(s) { sub(/^[ \t\r\n]+/, \"\", s); return s } function rtrim(s) { sub(/[ \t\r\n]+$/, \"\", s); return s } function trim(s)  { return rtrim(ltrim(s)); } BEGIN{FS=\"=\"} { if(NF==2) {print dt\",\"hst\",\"pfx\".\"trim(\$1)\",\"trim(\$2)} if(NF==1)  {print dt\",\"hst\",\"pfx\",\"trim(\$1)} } " '
 
 vmware-toolbox-cmd stat raw | xargs -n 1 -I '{}' sh -c 'prefix=$(echo "{}"| sed "s/^[ \t]*//" | sed "s/[ \t]*$//" | sed "s/ /./"); vmware-toolbox-cmd stat raw text {} | awk -v dt=$(date +\%d/\%m/\%YT\%H:\%M:\%S\%Z) -v hst=$HOSTNAME -v pfx=$prefix "function ltrim(s) { sub(/^[ \t\r\n]+/, \"\", s); return s } function rtrim(s) { sub(/[ \t\r\n]+$/, \"\", s); return s } function trim(s)  { return rtrim(ltrim(s)); } BEGIN{FS=\"=\"} {print dt\",\"hst\",\"pfx\".\"trim(\$1)\",\"trim(\$2)}" '
